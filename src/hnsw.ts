75ae3f2e8d51f04a3e7cb41e02ad76065e49ce5a
Yusuf Hilmi <177yusuf177@gmail.com>
2023-11-13 18:17:14 +0300

    feat: hnsw todos

---
+++ src/hnsw.ts
@@ -0,0 +1,5 @@
+// an experimental implementation of hnsw that doesn't rely on the hnsw binding libs which only works in browser or node
+// TODOS:
+// - bare bones
+// - find # layers and optimal params
+// - test the speed, accuracy, and memory usage
